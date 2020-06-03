0d383d699c900897c3ec05731fa3320ebe2160d0
camilo95951 <camilo.salgado95951@gmail.com>
2020-06-03 01:02:05 -0500

    algunos cambios

--- controlles/userController.js
+++ controlles/userController.js
@@ -1,16 +1,25 @@
 exports.createUser = (req,res,next) => {
     const name = req.body.name;
     const lastName = req.body.lastName;
-    console.log(name ,lastName);
+    const Age = req.body.Age;
+    const Email = req.body.Email;
+    const Pass = req.body.Pass;
 
-    res.status(200).json(
-        {
-            message: 'Usuario creado',
-            post: {
-                name,
-                lastName,
-            } 
-        }
-    );
-    
+    const data = {
+        name,
+        lastName,
+        Age,
+        Email,
+        Pass,
+      };
+/*  
+fs.writeFile(`${Data.now().toString()}.txt`,'Learn Node F5 module', function (err) {
+    if (err) throw err;
+    console.log('File is created successfully.');
+  }); 
+*/ 
+  red.status(200).json({
+    message: 'Usuario Creado',
+    id: Date.now(),
+});
 };
